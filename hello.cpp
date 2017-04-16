53d0e80568be72724a80066fd2b83ba1892e99aa
Andrei Mikhailov <encept@gmail.com>
2017-04-16 20:09:27 +0300

    added !!!

--- hello.cpp
+++ hello.cpp
@@ -3,6 +3,6 @@
 int main() {
     std::cout << "Hello" << std::endl;
     std::cout << "Testikus" << std::endl;
-    std::cout << "From Eclipse" << std::endl;
+    std::cout << "From Eclipse !!!" << std::endl;
     return 0;
 }
