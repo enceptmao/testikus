5339cc7f14f0de3ef38a83b905ed9f166cd8995a
Andrei Mikhailov <encept@gmail.com>
2017-04-16 20:20:28 +0300

    simplification

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,5 @@
 
 int main() {
     std::cout << "Hello" << std::endl;
-    std::cout << "Testikus" << std::endl;
-    std::cout << "From Eclipse !!!" << std::endl;
-    std::cout << "--------------------" << std::endl;
     return 0;
 }
